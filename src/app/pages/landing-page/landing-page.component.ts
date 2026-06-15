6e03d5969aae540b0cf67dddb7947d588148e83c
Martin Laus <sualkoo@gmail.com>
2026-06-15 14:36:51 +0200

    feat: refactor landing page to dynamically render sport icons and enable change detection

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { sportIcon } from '../../shared/utils/sport-icon';
+
+type FloatingSportIcon = {
+  modifier: string;
+  icon: string;
+};
 
 @Component({
   selector: 'app-landing-page',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterLink, MatIconModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  readonly floatingSportIcons: FloatingSportIcon[] = [
+    { modifier: 'soccer', icon: sportIcon('football') },
+    { modifier: 'basketball', icon: sportIcon('basketball') },
+    { modifier: 'tennis', icon: sportIcon('tennis') },
+    { modifier: 'hockey', icon: sportIcon('hockey') },
+    { modifier: 'esports', icon: sportIcon('esports') },
+    { modifier: 'motorsport', icon: sportIcon('motorsport') },
+  ];
+}
